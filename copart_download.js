43b8b0a9b290ebbde67980068197ae2f8a209c2f
patryk-swierlikowski <71077406+patryk-swierlikowski@users.noreply.github.com>
2021-12-03 01:18:27 +0100

    Longer time to execute

--- copart_download.js
+++ copart_download.js
@@ -2,7 +2,7 @@ const https = require('https');
 const fs = require('fs');
 
 const file = fs.createWriteStream("salesdata.csv");
-const request = https.get('https://inventory.copart.io/FTPLSTDM/salesdata.cgi?authKey=XMTY57AW', function(response) {
+const request = https.get('https://storage.bidspace.info/salesdata.csv', function(response) {
   response.pipe(file);
   console.log('Downloading CSV from Copart...');
 });
